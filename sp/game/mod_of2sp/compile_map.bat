83275ead1a9273e3aa9fc392a4a3f3c64f591573
Chris528 <chris52886@hotmail.com>
2014-07-25 05:08:15 -0400

    Added various materials.

--- sp/game/mod_of2sp/compile_map.bat
+++ sp/game/mod_of2sp/compile_map.bat
@@ -3,4 +3,5 @@ set filename=%~dpn1
 "%SPBASE2013BIN%\vbsp.exe" -game "%GameDir%" %filename%
 "%SPBASE2013BIN%\vvis.exe" -game "%GameDir%" %filename%
 "%SPBASE2013BIN%\vrad.exe" -game "%GameDir%" %filename%
+XCOPY "%filename%.bsp" "%GameDir%\maps" /D /y
 pause

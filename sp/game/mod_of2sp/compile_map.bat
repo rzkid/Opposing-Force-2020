8ab6a9e6ccc608e71188a3be0e39f68c1ec7758d
Chris528 <chris52886@hotmail.com>
2014-07-25 00:03:24 -0400

    Added various files.
    Added misc sounds, vmfs, models.
    Wrote a batch file for compiling vmf files.

---
+++ sp/game/mod_of2sp/compile_map.bat
@@ -0,0 +1,6 @@
+call %~dp0setvars
+set filename=%~dpn1
+"%SPBASE2013BIN%\vbsp.exe" -game "%GameDir%" %filename%
+"%SPBASE2013BIN%\vvis.exe" -game "%GameDir%" %filename%
+"%SPBASE2013BIN%\vrad.exe" -game "%GameDir%" %filename%
+pause
--- sp/game/mod_of2sp/run_hammer.bat
+++ sp/game/mod_of2sp/run_hammer.bat
@@ -1,3 +1,3 @@
-call setvars
+call setvars.bat
 
 "%SPBASE2013BIN%\hammer.exe" -game "%GameDir%"

1f7894009cc8898602c9ea03ad1659545a11ab20
Chris528 <chris52886@hotmail.com>
2014-08-07 14:00:58 -0400

    Added particles to vpk.

--- sp/game/mod_of2sp/create_vpk_files.bat
+++ sp/game/mod_of2sp/create_vpk_files.bat
@@ -2,6 +2,6 @@ call %~dp0setvars
 
 set VPK="%SPBASE2013%\bin\vpk.exe"
 
-python build_vpk.py %VPK% "of2_assets" "models" "materials" "sound"
+python build_vpk.py %VPK% "of2_assets" "models" "materials" "sound" "particles"
 
 pause
